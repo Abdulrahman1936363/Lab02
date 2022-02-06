cbaca5644652ff47b86c8ed431cba9969b1204cb
Abdulrahman1936363 <98707976+Abdulrahman1936363@users.noreply.github.com>
2022-02-06 12:20:59 +0300

    Add files via upload

---
+++ Logger.java
@@ -0,0 +1,41 @@
+
+package com.mycompany.lab02;
+
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+
+public class Logger {
+  DateTimeFormatter myFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss");
+  String currentDateTimeString = LocalDateTime.now().format(myFormatter);
+  private final String logFile = currentDateTimeString + ".txt";
+  private PrintWriter writer;
+  private static Logger log;
+
+  private Logger() {
+    try {
+      FileWriter fw = new FileWriter(logFile);
+      writer = new PrintWriter(fw, true);
+    }
+    catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+  public static Logger getlog(){
+      if (log == null) {
+          log = new Logger();
+          
+      }
+    return log;
+  }
+  public void info (String message) {
+    writer.println("INFO: " + message);
+  }
+  public void error (String message) {
+    writer.println("Error: " + message);
+  }
+}
+
